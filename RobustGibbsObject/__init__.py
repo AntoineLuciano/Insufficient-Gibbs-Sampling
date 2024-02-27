515901aea89a9766fe168d0b57b95840cda1fb9f
Antoine <antoine.luciano@gmail.com>
2024-02-27 14:48:24 +0100

    gitignore

--- RobustGibbsObject/__init__.py
+++
@@ -1,4 +0,0 @@
-from RobustGibbsObject.Distribution import Normal, Cauchy, Weibull, LogNormal, TranslatedLogNormal, TranslatedWeibull, GeneralizedPareto, Pareto, ReparametrizedTranslatedLogNormal,ReparametrizedTranslatedLogNormal
-
-from RobustGibbsObject.Model import NormalModel, CauchyModel, WeibullModel, LogNormalModel, TranslatedLogNormalModel, TranslatedWeibullModel, GeneralizedParetoModel
-#ReparametrizedTranslatedLogNormalModel, ReparametrizedTranslatedLogNormalModel
